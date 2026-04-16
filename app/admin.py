3e2c35eb7143563f584d0c818153bd55126bad66
Azik <azimoazmiddin@gmail.com>
2026-04-16 17:04:36 +0500

    add export in admin dashboard

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportMixin
+from django.contrib import admin
+from .models import Card
 
-# Register your models here.
+
+class CardResource(resources.ModelResource):
+    class Meta:
+        model = Card
+
+
+@admin.register(Card)
+class CardAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = CardResource
