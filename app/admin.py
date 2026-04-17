24c83e98beef47a2a11159804b39fb3891921e6b
Izzatillo <izzatillaasatullayev55@gmail.com>
2026-04-17 15:41:08 +0500

    add : fix formats

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ExportMixin
 from .models import Card
+from .utils import format_card_number, format_phone_number
 
 
 class CardResource(resources.ModelResource):
@@ -15,13 +16,12 @@ class CardAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('formatted_card', 'formatted_phone', 'balance', 'status', 'expire_date')
     
     def formatted_card(self, obj):
-        # 8600 0000 0000 0000 shu kabi format qilib berish uchun
-        return f"{obj.card_number[:4]} {obj.card_number[4:8]} {obj.card_number[8:12]} {obj.card_number[12:]}"
+        return format_card_number(obj.card_number)
     
     def formatted_phone(self, obj):
-        # +998 90 123 45 67 shu kabi format qilib berish uchun
-        return f"{obj.phone[:4]} {obj.phone[4:6]} {obj.phone[6:9]} {obj.phone[9:11]} {obj.phone[11:13]}"
+        return format_phone_number(obj.phone)
 
-    # bular yuqorida adminda ko'rinishi uchun
     formatted_card.short_description = "Card Number"
     formatted_phone.short_description = "Phone Number"
+    
+    
--- app/utils.py
+++ app/utils.py
@@ -13,3 +13,10 @@ def clean_card_number(card):
 def convert_date(mm_yy: str) -> str:
     date_obj = datetime.strptime(mm_yy, "%m-%y")
     return date_obj.strftime("%Y-%m-01")
+
+
+def format_card_number(card_number):
+    return f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+    
+def format_phone_number(phone_number):
+    return f"{phone_number[:4]} {phone_number[4:6]} {phone_number[6:9]} {phone_number[9:11]} {phone_number[11:13]}"
